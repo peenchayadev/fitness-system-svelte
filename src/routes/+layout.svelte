433eedb6a5ce13d59c6025312a5b1408c24cab7f
Peenchayakorn <148639119+peenchayadev@users.noreply.github.com>
2024-08-09 02:11:12 +0700

    setup-project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,15 @@
 <script>
-	import Header from './Header.svelte';
-	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
+	import Sidebar from '../components/Sidebar.svelte';
 </script>
 
-<div class="app">
-	<Header />
+<Navbar />
+<Sidebar />
 
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+<div class="content-wrapper">
+	<section class="content">
+		<div class="container-fluid">
+			<slot />
+		</div>
+	</section>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,50 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import Swal from "sweetalert2";
+
+	let username = '';
+	let password = '';
+
+	async function SignIn() {
+		try {
+			if (username =='' || password == ''){
+				Swal.fire({
+				title : 'ตรวจสอบข้อมูล',
+				text: 'โปรดกรอกข้อมูลด้วย',
+				icon: 'warning'
+			})
+			} else {
+				const payload = {
+					username: username,
+					password: password
+				}
+
+				const res = await axios.post('http://localhost:3000/api/user/signin',payload)
+			}
+		} catch (e) {
+			Swal.fire({
+				title : 'error',
+				text: e.message,
+				icon: 'error'
+			})
+		}
+	}
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div class="crad mt-3">
+	<div class="card-header bg-primary">SignIn to BackOffice of Fitness center</div>
+	<div class="card-body">
+		<div>
+			<div>Username</div>
+			<input class="form-control" bind:value={username}>
+		</div>
+		<div class="mt-3">
+			<div>Password</div>
+			<input class="form-control mt-3" type='password' bind:value={password}>
+		</div>
+		<div class="mt-3">
+			<button class="btn btn-primary" on:click={()=> SignIn()}>
+				<i class="fa fa-check mt-2">Sign In now</i>
+			</button>
+		</div>
+	</div>
+</div>
